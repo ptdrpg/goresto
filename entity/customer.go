c0545554b3b2e0b40f6668c110fb12e379debeae
ptdrpg <pagielamizael@gmail.com>
2024-05-28 10:36:00 +0300

    add field point

--- entity/customer.go
+++ entity/customer.go
@@ -4,8 +4,9 @@ type Customer struct {
 	ID           uint   `gorm:"primary_key" json:"id"`
 	Name         string `json:"name"`
 	Email        string `json:"email"`
-	Phone_number int  `json:"phone_number"`
+	Phone_number int    `json:"phone_number"`
 	Address      string `json:"address"`
 	Age          string `json:"age"`
 	Gender       string `json:"gender"`
+	Point        int    `json:"point"`
 }
